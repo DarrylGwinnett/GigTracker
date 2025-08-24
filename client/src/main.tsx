538ab70136a44f57b4c1324264e87c5ac5fe0f88
DarrylGwinnett <github@darrylgwinnett.com>
2025-08-24 15:53:39 +0100

    clean up files

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-<<<<<<< HEAD
 import './app/layout/index.css'
 import App from './app/layout/App.tsx'
-=======
-import './index.css'
-import App from './App.tsx'
->>>>>>> origin/main
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
