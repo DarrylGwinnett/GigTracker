936e8161ca637175e17ef24fbb44d6de01ee2de8
DarrylGwinnett <github@darrylgwinnett.com>
2025-08-24 13:39:35 +0100

    Added basic gig form

--- client/src/features/Gigs/Dashboard/GigDashboard.tsx
+++ client/src/features/Gigs/Dashboard/GigDashboard.tsx
@@ -1,25 +1,32 @@
-import { Grid2} from "@mui/material";
+import { Grid2 } from "@mui/material";
 import GigList from "./GigList";
 import GigDetails from "../Details/GigDetails";
+import GigForm from "../form/GigForm";
 
-type Props= {
-    gigs: Gig[];
-    selectGig:(id:string) => void
-    cancelSelectGig: () => void
-    selectedGig?: Gig
-}
+type Props = {
+  gigs: Gig[];
+  selectGig: (id: string) => void;
+  cancelSelectGig: () => void;
+  selectedGig?: Gig;
+};
 
-export default function GigDashboard({gigs, selectGig, cancelSelectGig, selectedGig}: Props) {
+export default function GigDashboard({
+  gigs,
+  selectGig,
+  cancelSelectGig,
+  selectedGig,
+}: Props) {
   return (
     <Grid2 container>
       <Grid2 size={7}>
-      <GigList gigs={gigs}
-      selectGig={selectGig}
-      />
+        <GigList gigs={gigs} selectGig={selectGig} />
       </Grid2>
-      <Grid2 size={5}>{selectedGig && <GigDetails gig={selectedGig}
-      cancelSelectGig={cancelSelectGig}
-      />}</Grid2>
+      <Grid2 size={5}>
+        {selectedGig && (
+          <GigDetails gig={selectedGig} cancelSelectGig={cancelSelectGig} />
+        )}
+        <GigForm/>
+      </Grid2>
     </Grid2>
   );
 }
