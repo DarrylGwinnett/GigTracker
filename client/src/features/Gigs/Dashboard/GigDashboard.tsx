89589ede3af7823d922c062ae8f74b00bc40e370
DarrylGwinnett <github@darrylgwinnett.com>
2025-08-30 13:10:11 +0100

    Style the gig filters

--- client/src/features/Gigs/Dashboard/GigDashboard.tsx
+++ client/src/features/Gigs/Dashboard/GigDashboard.tsx
@@ -1,18 +1,19 @@
 import { Grid2, Typography } from "@mui/material";
 import GigList from './GigList.tsx'
 import { useGigs } from "../../../lib/hooks/useGigs";
+import GigFilters from "./GigFilters.tsx";
 
 
 export default function GigDashboard() {
   const { gigs, isPending } = useGigs();
   if(!gigs || isPending) return <Typography>Loading...</Typography>
   return (
-    <Grid2 container>
-      <Grid2 size={7}>
+    <Grid2 container spacing={3}>
+      <Grid2 size={8}>
         <GigList />
       </Grid2>
-      Gig Filter
-      <Grid2 size={5}>
+      <Grid2 size={4}>        
+      <GigFilters />
       </Grid2>
     </Grid2>
   );
