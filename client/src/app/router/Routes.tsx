8d30c98073406ce52729f6081bcc2dbdc3082639
Darryl <48260694+DarrylGwinnett@users.noreply.github.com>
2025-09-07 19:58:49 +0100

    UI error handling (#14)

--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -1,9 +1,12 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, Navigate } from "react-router";
 import App from "../layout/App";
 import HomePage from "../../features/home/HomePage";
 import GigDashboard from "../../features/gigs/dashboard/GigDashboard";
 import GigForm from "../../features/gigs/form/GigForm";
 import GigDetailPage from "../../features/gigs/details/GigDetailPage";
+import TestErrors from "../../features/errors/TestErrors";
+import NotFound from "../../features/errors/NotFound";
+import ServerError from "../../features/errors/ServerError";
 
 export const router = createBrowserRouter([
     {
@@ -15,6 +18,11 @@ export const router = createBrowserRouter([
             { path: 'createGig', element:<GigForm key='create'/> },
             { path: 'manage/:id', element:<GigForm key='edit'/> },
             { path: 'gigs/:id', element:<GigDetailPage/> },
+            { path: 'errors', element:<TestErrors/> },
+            { path: 'notFound', element:<NotFound/> },
+            { path: 'serverError', element:<ServerError/> },
+            { path: '*', element:<Navigate replace to='notFound'/> },
         ]
     }
 ])
+
