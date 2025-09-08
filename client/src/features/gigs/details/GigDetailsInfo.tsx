3ed4d5dc4d74f49f69c9394c3082db81b9a865c4
DarrylGwinnett <github@darrylgwinnett.com>
2025-09-08 12:46:52 +0100

    Add map component

--- client/src/features/gigs/details/GigDetailsInfo.tsx
+++ client/src/features/gigs/details/GigDetailsInfo.tsx
@@ -1,13 +1,19 @@
 
 import { CalendarToday, Info, Place } from "@mui/icons-material";
-import { Divider, Grid2, Paper, Typography } from "@mui/material";
+import { Box, Button, Divider, Grid2, Paper, Typography } from "@mui/material";
 import { formatDate } from "../../../lib/util/util";
+import type { Gig } from "../../../lib/types";
+import { useState } from "react";
+import MapComponent from "../../../app/layout/shared/MapComponent";
 
 type Props = {
     gig: Gig;
 }
 
 export default function GigDetailsInfo({gig}: Props) {
+    const [mapOpen, setMapOpen] = useState(false);
+
+
     return (
         <Paper sx={{ mb: 2 }}>
 
@@ -34,12 +40,18 @@ export default function GigDetailsInfo({gig}: Props) {
                 <Grid2 size={1}>
                     <Place color="info" fontSize="large" />
                 </Grid2>
-                <Grid2 size={11}>
+                <Grid2 size={11} display='flex' justifyContent={'space-between'} alignItems={'centre'} >
                     <Typography>
                         {gig.venue}, {gig.city}
                     </Typography>
+                    <Button onClick={() => setMapOpen(!mapOpen)}>
+                        {mapOpen ? 'Hide Map' : 'Show Map'}
+                    </Button>
                 </Grid2>
             </Grid2>
+            {mapOpen && (<Box sx={{ height: 400, width: '100%', display: 'block', z: 1000 }}>
+                <MapComponent position={[gig.latitude, gig.longitude]} venue={gig.venue}/>
+            </Box>)}
         </Paper>
     )
 }
